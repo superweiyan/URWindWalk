ab7c36aba446a67c35744b4e52651d3d3a7e9e55
superweiyan <superweiyan@163.com>
2016-12-04 23:02:49 +0800

    Initial Commit

---
+++ URWindWalk/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  URWindWalk
+//
+//  Created by weiyan on 04/12/2016.
+//  Copyright © 2016 weiyan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ URWindWalk/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  URWindWalk
+//
+//  Created by weiyan on 04/12/2016.
+//  Copyright © 2016 weiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
